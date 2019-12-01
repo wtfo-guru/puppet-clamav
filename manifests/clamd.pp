a2dd10f257acdba59234671ffa503ed4568def6f
Quien Sabe <qs5779@mail.com>
2019-12-01 17:27:22 -0500

    Passing all os'es'

--- manifests/clamd.pp
+++ manifests/clamd.pp
@@ -5,6 +5,7 @@
 class clamav::clamd {
 
   $config_options = $clamav::_clamd_options
+  $db_directory = $clamav::params::clamd_default_databasedirectory
 
   # NOTE: In RedHat and Archlinux this is part of the base clamav_package
   if $clamav::clamd_package {
@@ -20,6 +21,29 @@ class clamav::clamd {
   }
   else {
     $service_subscribe = File['clamd.conf']
+  }
+
+  case $facts['os']['family'] {
+    'RedHat': {
+      if $facts['operatingsystemmajrelease'] =~ /^7/ {
+        # solve this chicken/egg problem, clamd can't start until database files are preset
+        # for redhat 7 those are in the clamav-data packages, which apparently isn't required by any other package
+        # files would be obtained from the first freshclam run which might be 3 hours after first install
+        ensure_packages(['clamav-data'], {'ensure' => 'installed', 'before' => Service['clamd']})
+      }
+    }
+    'Debian': {
+      # clamav-daemon not start the service without this file, I think it used
+      # to be provided by freshclam, but not sure
+      file {"${db_directory}/daily.inc":
+        ensure => file,
+        mode   => '0644',
+        owner  => $clamav::user,
+        group  => $clamav::group,
+        before => Service['clamd'],
+      }
+    }
+    default: {}
   }
 
   file { 'clamd.conf':
@@ -38,16 +62,5 @@ class clamav::clamd {
     hasrestart => true,
     hasstatus  => true,
     subscribe  => $service_subscribe,
-  }
-
-  if lookup('clamav::clamd_requires_daily_inc', Boolean, first, false) {
-    # some systems will not start the service without this file
-    file {"${clamav::params::clamd_default_databasedirectory}/daily.inc":
-      ensure => file,
-      mode   => '0644',
-      owner  => $clamav::user,
-      group  => $clamav::group,
-      before => Service['clamd'],
-    }
   }
 }
